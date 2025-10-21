806ec55ed95004c7cfac2dbdd642b5cd40138ed9
gamsriwan-tech <gamsriwan@gmail.com>
2025-10-21 16:10:19 +0700

    Ant walk point A-B

--- Assets/Scripts/Ant.cs
+++ Assets/Scripts/Ant.cs
@@ -2,17 +2,44 @@ using UnityEngine;
 
 public class Ant : Enemy
 {
-    [SerializeField] Vector2 velosity;
-    public Transform[] MovePoint;
+    public Vector2 velosity;
+    public Transform[] movePoints;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         base.Initialize(20);
+
+        DamageHit = 20;
+        velosity = new Vector2(-1.0f, 0.0f);
     }
 
     public override void Behavior()
     {
-        throw new System.NotImplementedException();
+        //move from current position
+        rb.MovePosition(rb.position + velosity * Time.fixedDeltaTime);
+        //move left และเกินขอบซ้าย
+        if (velosity.x < 0 && rb.position.x <= movePoints[0].position.x)
+        {
+            Flip();
+        }
+        //move right และเกินขอบขวา
+        if (velosity.x > 0 && rb.position.x >= movePoints[1].position.x)
+        {
+            Flip();
+        }
+
+    }
+    public void Flip()
+    {
+        velosity.x *= -1; //change direction of movement
+                          //Flip the image
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
+    private void FixedUpdate()
+    {
+        Behavior();
     }
     // Update is called once per frame
     void Update()
