17e60da315b9b3c51a56c70d6d3e0afa9bb696d2
gamsriwan-tech <Gamsriwan@gmail.com>
2025-10-28 16:34:09 +0700

    FinishButNotHealBar

--- Assets/Scripts/Croccodile.cs
+++ Assets/Scripts/Croccodile.cs
@@ -1,20 +1,35 @@
 using UnityEngine;
 
-public class Croccodile : Enemy
+public class Croccodile : Enemy , IsShootable 
 {
     [SerializeField] private float atkRange;
     public Player player; //target to atk
+
+    [field: SerializeField] public GameObject Bullet {  get; set; }
+    [field: SerializeField] public Transform ShootPoint { get; set; }
+    public float ReloadTime { get; set; }
+    public float WaitTime { get; set; }
+
+
     void Start()
     {
         base.Initialize(50);
         DamageHit = 30;
+
         //set atk range and target
         atkRange = 6.0f;
         player = GameObject.FindFirstObjectByType<Player>();
+
+        //set times for Rock
+        WaitTime = 0.0f;
+        ReloadTime = 5.0f;
+
+
     }
 
     private void FixedUpdate()
     {
+        WaitTime += Time.fixedDeltaTime;
         Behavior();
     }
     public override void Behavior()
@@ -29,7 +44,15 @@ public class Croccodile : Enemy
     }
     public void Shoot()
     {
-        Debug.Log($"{this.name} shoots rock to the {player.name}!");
+        if (WaitTime >= ReloadTime)
+        {
+            anim.SetTrigger("Shoot");
+            var bullet = Instantiate(Bullet, ShootPoint.position, Quaternion.identity);
+            Rock rock = bullet.GetComponent<Rock>();
+            rock.InitWeapon(30 , this);
+            WaitTime = 0;
+        }
+        
     }
 
         // Update is called once per frame
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,11 +1,25 @@
 using UnityEngine;
 
-public class Player : Character
+public class Player : Character , IsShootable
 {
+    [field: SerializeField] public GameObject Bullet {  get; set; }
+    [field: SerializeField] public Transform ShootPoint { get; set; }
+    public float ReloadTime { get; set; }
+    public float WaitTime { get; set; }
+
+    
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         base.Initialize(100);
+        ReloadTime = 1.0f;
+        WaitTime = 0.0f;
+    }
+
+    private void FixedUpdate()
+    {
+        WaitTime += Time.fixedDeltaTime;
     }
 
     public void OnHitWith(Enemy enemy)
@@ -20,12 +34,26 @@ public class Player : Character
         { 
            //Takedamage
            OnHitWith(enemy);
+            Debug.Log($"{this.name} collides with {enemy.name}!");
         }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Shoot();
+    }
+    public void Shoot()
+    {
+        if (Input.GetButtonDown("Fire1") && WaitTime >= ReloadTime)
+        {
+            var bullet = Instantiate(Bullet , ShootPoint.position, Quaternion.identity);
+            Banana banana = bullet.GetComponent<Banana>();
+            if ( banana != null)
+            {
+                banana.InitWeapon(20 , this);
+                WaitTime = 0.0f;
+            }
+        }
     }
 }
